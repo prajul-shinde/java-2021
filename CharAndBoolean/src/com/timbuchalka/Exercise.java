8ff9561be3e55e81274bf61712898434873e6d47
prajul-shinde <prajulshinde@gmail.com>
2021-03-29 21:25:49 +0530

    char and boolean data type

---
+++ CharAndBoolean/src/com/timbuchalka/Exercise.java
@@ -0,0 +1,8 @@
+package com.timbuchalka;
+
+public class Exercise {
+    public static void main(String[] args) {
+        char registeredSymbol='\u00AE';
+        System.out.println("registered symbol was: "+registeredSymbol);
+    }
+}
---
+++ CharAndBoolean/src/com/timbuchalka/Main.java
@@ -0,0 +1,12 @@
+package com.timbuchalka;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //width of char is 16 bit
+         char myChar='\u00a9';
+        System.out.println("unicode output was: "+myChar);
+
+        boolean isMale=true;
+    }
+}
