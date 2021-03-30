09f0a1730a325f463373505393643b9043767a47
prajul-shinde <prajulshinde@gmail.com>
2021-03-30 12:31:17 +0530

    keywords, statements and indentatons

---
+++ StatementsWhitespceAndIndenting/src/com/timbuchalka/Main.java
@@ -0,0 +1,15 @@
+package com.timbuchalka;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // indenting makes it easier to understand logical flow of program
+        int myVariable=50;
+        myVariable++;
+        myVariable--;
+        System.out.println("This is a test");
+        System.out.println("this is" +
+                "another" +
+                "still more.");
+    }
+}
