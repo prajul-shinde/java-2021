2399ee69e22a0bbf7f36059e4c821003bd93934d
prajul-shinde <prajulshinde@gmail.com>
2021-04-08 15:19:21 +0530

    encapsulation challenge

---
+++ Encapsulation/src/com/timbuchalka/challenge/Main.java
@@ -0,0 +1,13 @@
+package com.timbuchalka.challenge;
+
+public class Main {
+    public static void main(String[] args) {
+        Printer printer=new Printer(50,true);
+        System.out.println("initial page count ="+printer.getPagesPrinted());
+        int pagesPrinted=printer.printPages(4);
+        System.out.println("pages printed was "+pagesPrinted+" new total count for printer "+printer.getPagesPrinted());
+        pagesPrinted=printer.printPages(2);
+        System.out.println("pages printed was "+pagesPrinted+" new total count for printer "+printer.getPagesPrinted());
+
+    }
+}
